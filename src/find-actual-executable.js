8ed277157762c41d45a55465eb10440f608a3b47
Paul Betts <paul@paulbetts.org>
2016-02-29 14:27:14 -0800

    Steal findActualExecutable from Surf

---
+++ src/find-actual-executable.js
@@ -0,0 +1,47 @@
+'use strict';
+
+const path = require('path');
+const sfs = require('fs');
+
+function findActualExecutable(fullPath, args) {
+  // POSIX can just execute scripts directly, no need for silly goosery
+  if (process.platform !== 'win32') return { cmd: fullPath, args: args };
+
+  // NB: When you write something like `surf-client ... -- surf-build` on Windows,
+  // a shell would normally convert that to surf-build.cmd, but since it's passed
+  // in as an argument, it doesn't happen
+  if (!sfs.existsSync(fullPath)) {
+    const possibleExts = ['.exe', '.bat', '.cmd', '.ps1'];
+    let realExecutable = possibleExts.find((x) => sfs.existsSync(`${fullPath}${x}`));
+
+    if (realExecutable) {
+      return findActualExecutable(realExecutable, args);
+    }
+  }
+
+  if (fullPath.match(/\.ps1$/i)) {
+    let cmd = path.join(process.env.SYSTEMROOT, 'System32', 'WindowsPowerShell', 'v1.0', 'PowerShell.exe');
+    let psargs = ['-ExecutionPolicy', 'Unrestricted', '-NoLogo', '-NonInteractive', '-File', fullPath];
+
+    return { cmd: cmd, args: psargs.concat(args) };
+  }
+
+  if (fullPath.match(/\.(bat|cmd)$/i)) {
+    let cmd = path.join(process.env.SYSTEMROOT, 'System32', 'cmd.exe');
+    let cmdArgs = ['/C', fullPath];
+
+    return { cmd: cmd, args: cmdArgs.concat(args) };
+  }
+
+  if (fullPath.match(/\.(js)$/i)) {
+    let cmd = process.execPath;
+    let nodeArgs = [fullPath];
+
+    return { cmd: cmd, args: nodeArgs.concat(args) };
+  }
+
+  // Dunno lol
+  return { cmd: fullPath, args: args };
+}
+
+module.exports = findActualExecutable;
